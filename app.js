0526e426a3d7697f1b23cbaa91b92d77d5a37702
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-07-05 08:59:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 
+import "./App.css";
+import React, { useState } from "react";
+import Popup from "./Popup";
+import Counter from "./Counter";
+import Users from "./data";
+import Table from './Table';
+import Paginate from './Paginate'
