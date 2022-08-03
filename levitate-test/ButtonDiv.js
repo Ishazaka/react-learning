00e277d6232d4f14b81e0f2dd61bc5dfae1e5121
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-08-02 22:23:07 -0400

    Create ButtonDiv.js

---
+++ levitate-test/ButtonDiv.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Button from './Button'
+
+const ButtonDiv = ({countit}) => {
+  return (
+    <div>
+        <Button countit={countit}/>
+        
+    </div>
+  )
+}
+
+export default ButtonDiv
