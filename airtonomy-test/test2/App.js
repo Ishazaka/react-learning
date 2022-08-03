1af0b39daf82de938cb0f6a513df7e83b110eebd
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-08-02 22:16:54 -0400

    Update App.js

--- airtonomy-test/test2/App.js
+++ airtonomy-test/test2/App.js
@@ -1,1 +1,39 @@
 
+import { useState , useEffect } from 'react';
+import './App.css';
+import Studentdata from './component/Studentdata';
+
+function App() {
+  const [alldata, setAlldata] = useState();
+
+  async function getdata(){
+      try{
+          const  url = "https://api.hatchways.io/assessment/students";
+          const response = await fetch(url);
+          const data = await response.json();
+          console.log(data)
+          const studentinfos = data.students
+          setAlldata(studentinfos);
+
+      } catch (error){
+       console.log(error)
+      }
+     
+    }
+
+ 
+
+  useEffect(() => {
+    getdata();
+  
+  }, [])
+
+
+  return (
+   <>
+< Studentdata  alldata={alldata} />
+   </>
+  );
+}
+
+export default App;
